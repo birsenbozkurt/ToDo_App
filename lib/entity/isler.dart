8cb848de430f4b0c43e2a22dca08a1f968acfc8e
birsenbozkurt <birsenbozkurt4041@gmail.com>
2022-09-12 13:37:15 +0300

    design

---
+++ lib/entity/isler.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Isler {
+  int yapilacak_id;
+  String yapilacak_is;
+  String is_no;
+  Isler({
+    required this.yapilacak_id,
+    required this.yapilacak_is,
+    required this.is_no,
+  });
+}
